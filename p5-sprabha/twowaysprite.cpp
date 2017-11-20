56ef116fd50aac7466f7ce6a51329ed2e3ee8da7
Shreyas Prabhakar <shreyas.p342@gmail.com>
2017-11-20 12:59:00 -0500

    started project 5

---
+++ p5-sprabha/twowaysprite.cpp
@@ -0,0 +1,84 @@
+#include "twowaysprite.h"
+#include "gamedata.h"
+#include "renderContext.h"
+
+void twowaySprite::advanceFrame(Uint32 ticks) {
+	timeSinceLastFrame += ticks;
+	if (timeSinceLastFrame > frameInterval) {
+    currentFrame = (currentFrame+1) % numberOfFrames;
+		timeSinceLastFrame = 0;
+	}
+}
+
+twowaySprite::twowaySprite( const std::string& name) :
+  Drawable(name, 
+           Vector2f(Gamedata::getInstance().getXmlInt(name+"/startLoc/x"), 
+                    Gamedata::getInstance().getXmlInt(name+"/startLoc/y")), 
+           Vector2f(Gamedata::getInstance().getXmlInt(name+"/speedX"),
+                    Gamedata::getInstance().getXmlInt(name+"/speedY"))
+           ),
+  imagesRight( RenderContext::getInstance()->getImages(name) ),
+  imagesLeft( RenderContext::getInstance()->getImages(name+"Left") ),
+  images( RenderContext::getInstance()->getImages(name) ),
+
+  currentFrame(0),
+  numberOfFrames( Gamedata::getInstance().getXmlInt(name+"/frames") ),
+  frameInterval( Gamedata::getInstance().getXmlInt(name+"/frameInterval")),
+  timeSinceLastFrame( 0 ),
+  worldWidth(Gamedata::getInstance().getXmlInt("world/width")),
+  worldHeight(Gamedata::getInstance().getXmlInt("world/height"))
+{ }
+
+twowaySprite::twowaySprite(const twowaySprite& s) :
+  Drawable(s), 
+  imagesRight(s.imagesRight),
+  imagesLeft(s.imagesLeft),
+  images(s.images),
+  currentFrame(s.currentFrame),
+  numberOfFrames( s.numberOfFrames ),
+  frameInterval( s.frameInterval ),
+  timeSinceLastFrame( s.timeSinceLastFrame ),
+  worldWidth( s.worldWidth ),
+  worldHeight( s.worldHeight )
+  { }
+
+twowaySprite& twowaySprite::operator=(const twowaySprite& s) {
+  Drawable::operator=(s);
+  imagesRight = (s.imagesRight);
+  imagesLeft = (s.imagesLeft);
+  images = (s.images);
+  currentFrame = (s.currentFrame);
+  numberOfFrames = ( s.numberOfFrames );
+  frameInterval = ( s.frameInterval );
+  timeSinceLastFrame = ( s.timeSinceLastFrame );
+  worldWidth = ( s.worldWidth );
+  worldHeight = ( s.worldHeight );
+  return *this;
+}
+
+void twowaySprite::draw() const { 
+  images[currentFrame]->draw(getX(), getY(), getScale());
+}
+
+void twowaySprite::update(Uint32 ticks) { 
+  advanceFrame(ticks);
+
+  Vector2f incr = getVelocity() * static_cast<float>(ticks) * 0.001;
+  setPosition(getPosition() + incr);
+
+  if ( getY() < 0) {
+    setVelocityY( fabs( getVelocityY() ) );
+  }
+  if ( getY() > worldHeight-getScaledHeight()) {
+    setVelocityY( -fabs( getVelocityY() ) );
+  }
+
+  if ( getX() < 0) {
+    setVelocityX( fabs( getVelocityX() ) );
+    images = imagesRight;
+  }
+  if ( getX() > worldWidth-getScaledWidth()) {
+    setVelocityX( -fabs( getVelocityX() ) );
+    images = imagesLeft;
+  }  
+}
